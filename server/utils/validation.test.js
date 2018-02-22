ade5d04013d1c5cc83abc2db13087a09cd34665e
Rhyzko <valentin.martin@outlook.com>
2018-02-22 22:47:14 +0100

    Add users class and update user list + test

---
+++ server/utils/validation.test.js
@@ -0,0 +1,18 @@
+const expect = require('expect');
+
+const {isRealString} = require('./validation');
+
+describe('isRealString', () => {
+    it('should reject non-string value', () => {
+        var res = isRealString(123);
+        expect(res).toBe(false);
+    });
+    it('should reject string with only space', () => {
+        var res = isRealString('   ');
+        expect(res).toBe(false);
+    });
+    it('should allow string with non-space characters', () => {
+        var res = isRealString('  Hello  ');
+        expect(res).toBe(true);
+    });
+});
